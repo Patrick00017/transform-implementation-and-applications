0304136101b58f312887da3bc16623a909b3ee3a
admin <742679653@qq.com>
2022-10-25 16:23:42 +0800

    test: test the multiprocess in pytorch.

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-from multiprocessing import Process, Lock
+from multiprocessing import Process, Lock, Array
+import torch
 
-def f(l, i):
-    l.acquire()
-    try:
-        print('hello world', i)
-    finally:
-        l.release()
+
+def f(l, i, arr):
+    box = torch.rand(4)
+    arr[i, :] = box
+    print(f'p{i} finish')
 
 if __name__ == '__main__':
     lock = Lock()
+    shape = (3, 4)
+    arr = torch.zeros(size=shape).share_memory_()
+    print(f'before: {arr}')
+    process = []
+    for num in range(3):
+        a = Process(target=f, args=(lock, num, arr))
+        a.start()
+        process.append(a)
+    for p in process:
+        p.join()
 
-    for num in range(10):
-        Process(target=f, args=(lock, num)).start()
-
-
+    print(f'after: {arr}')
